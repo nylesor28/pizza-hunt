0a4002a0aaf92e2e88a72a7a37e65bc0227a1834
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-09-26 19:51:30 -0400

    create comment  model and routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,16 +4,28 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => {
-        console.log(err);
-        res.sendStatus(400);
-      });
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => {
+          console.log(err);
+          res.sendStatus(400);
+        });
   },
 
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
